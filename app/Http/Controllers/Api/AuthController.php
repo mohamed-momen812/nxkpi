a8b8160f87493f3b796e5cab4ecdaaca184c785e
Hossam Hamed <h.hamed@neoxero.com>
2023-08-06 18:57:58 +0300

    create dashboard model and migration files

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
             return response()->json($validator->errors(), 422);
         }
         if (! $token = auth()->attempt($validator->validated())) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Credintials fail'], 401);
         }
         return $this->createNewToken($token);
     }
