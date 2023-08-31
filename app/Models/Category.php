044b8ff70493d3001774955dfad749476f621005
Hossam Hamed <h.hamed@neoxero.com>
2023-08-31 15:54:07 +0300

    edit on category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
 {
     use HasFactory;
+    use HasFactory , HasTranslations;
 
+    public $translatable = ['name'];
     protected $fillable =['name', 'user_id', 'sort_order', 'created_at', 'updated_at'];
 
     public function user()
